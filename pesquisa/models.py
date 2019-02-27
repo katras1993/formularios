2f0c0f6b0f86211687407ff0e41b006ea00e33b7
kauana <kauanatamara@hotmail.com>
2019-02-26 21:57:43 -0300

    inclusão de models de pesquisa

--- pesquisa/models.py
+++ pesquisa/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Pergunta(models.Model):
+    texto = models.CharField()
+    data = models.DateField()
+
+class Alternativa(models.Model):
+    texto = models.CharField()
+    votos = models.IntegerField()
